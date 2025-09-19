4a4e67303c5e76560a320ce47c3b6b253ea403ca
AiWithRajesh <choudharyrajesh729@gmail.com>
2025-09-19 17:53:58 +0530

    deleted files due to name change

--- static-site-generation/src/components/LikeButton.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-import React from "react";
-
-export default function LikeButton() {
-  const [likes, setLikes] = React.useState(0);
-  return <button onClick={() => setLikes(likes + 1)}>Likes: {likes}</button>;
-}
